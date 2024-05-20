3b4977a31b4940eeba9539acdc1a510c9f5f20dd
ecary89 <116216639+ecary89@users.noreply.github.com>
2024-05-19 20:09:44 -0700

    Updated load in animations
    updated load in animations based on new design

--- load-in-animations.js
+++ load-in-animations.js
@@ -1,35 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
+document.addEventListener('DOMContentLoaded', function() {
     // Get all elements with the class 'fade-in'
-        var elements = document.querySelectorAll('.fade-in');
-    
-        // Options for the IntersectionObserver
-        var options = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.5,
-        };
-    
-        // Callback function to handle the intersection
-        function handleIntersection(entries, observer) {
-        entries.forEach(function(entry) {
-            console.log('Intersection triggered');
-            if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-            observer.unobserve(entry.target);
-            }
-        });
-        }
-    
-        // Create an IntersectionObserver with the callback and options
-        var observer = new IntersectionObserver(handleIntersection, options);
-    
-        // Observe each element
-        elements.forEach(function (element) {
-            var duration = element.dataset.duration || 1; // Default duration is 1s
-            element.style.transition = `opacity ${duration}s ease-in-out`;
-            observer.observe(element);
-        });
-        console.log("load-in-animations.js loaded");
-    }, 100);
-  });
+    var elements = Array.from(document.querySelectorAll('.fade-in'));
+
+    // Sort elements based on their data-order attribute
+    elements.sort(function(a, b) {
+        return a.getAttribute('data-order') - b.getAttribute('data-order');
+    });
+
+    // Define the initial delay before the first element starts to fade in
+    var initialDelay = 0.3; // in seconds
+
+    // Apply fade-in effect in the specified order
+    elements.forEach(function(element, index) {
+        var delay = initialDelay + index * 0.3; // Adjust the delay as needed (0.5 seconds between each element)
+        element.style.transitionDelay = `${delay}s`;
+        setTimeout(() => {
+            element.classList.add('visible');
+        }, delay * 1000); // Convert seconds to milliseconds for setTimeout
+    });
+});
